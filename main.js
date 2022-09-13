c4cae1bc1a185ec5861c21292c1069a9e01d6119
Zulfiqar Ali <zulfiqarqureshi619@gmail.com>
2022-09-13 17:16:59 +0500

    modulerize the library into smaller files and helper funcs, also implement request 'cancel' func

--- main.js
+++ main.js
@@ -1,37 +1,50 @@
-const Router = require('./router')
+const Router = require('./lib/router')
 const app = new Router()
 
 function mid(req, res, next) {
-    let bool = {
+    let obj = {
         message: 'data from mid middleware function',
         isPresent: false,
     }
-    next(bool)
+    // next(obj)
+    // res.json(obj)
+    req.local = 'worked'
+    next(obj)
 }
 
-function mid2(req, res, next) {
+function mid2(req, res, next, cancel) {
     if (req.nextData.isPresent) {
         req.nextData.anotherField = 'Created dynamically inside mid2'
-        res.json(req.nextData)
-    } else {
         next()
+    } else {
+        cancel((req) => {
+            console.log(req.path)
+            res.json('worked')
+        })
     }
 }
+
 app.get('/new', (req, res) => {
     res.json({
         message: 'worked again on json method :D',
     })
 })
 
-app.patch('/new', (req, res) => {
-    res.status(200).json(req.body)
+app.post('/new', mid, (req, res) => {
+    res.status(200).json({
+        body: req.body,
+    })
 })
 
 app.get(`/`, mid, mid2, (req, res) => {
-    res.json({
-        message: 'index on json method',
-        nextData: req.nextData,
-    })
+    try {
+        res.json({
+            message: 'index on json method',
+            nextData: req.nextData,
+        })
+    } catch (e) {
+        res.json(e)
+    }
 })
 
 app.Listen(8080, () => console.log(`Server started on port 8080`))
