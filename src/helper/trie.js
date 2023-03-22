dd24d768673354fdeebf76f4f9248a482e38e8e0
Zulfiqar Ali Qureshi <zulfiqarqureshi619@gmail.com>
2023-03-23 03:10:09 +0500

    add: support for routine.config.js file to tweak router settings

--- src/helper/trie.js
+++ src/helper/trie.js
@@ -1,8 +1,24 @@
-const trieRouter = require('find-my-way')({
-    defaultRoute: (req, res) => {
-        res.statusCode = 404
-        res.end()
-    },
-})
+const path = require('path')
+
+let config = {
+    allowUnsafeRegex: false,
+    caseSensitive: false,
+    maxParamLength: 100,
+    maxParamCount: 32,
+    wildcardPolicy: 'free',
+    mergeParams: true,
+    allowUnsafePath: false,
+}
+
+try {
+    const configFile = path.resolve(process.cwd(), 'routine.config.js')
+    const conf = require(configFile)
+    config = {
+        ...config,
+        ...conf,
+    }
+} catch (err) {}
+
+const trieRouter = require('find-my-way')(config)
 
 module.exports = trieRouter
